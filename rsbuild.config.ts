27aff2b01f7661f59fd2cb357b3bb32d3be0973d
h5lianfu <h5lianfu@gmail.com>
2025-10-12 20:06:37 -0400

    support scss

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from '@rsbuild/plugin-sass';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(), pluginSass()],
   server: { port: 5500 },
   html: { template: './public/index.html' },
+  resolve: {
+    alias: {
+      '@': './src',
+      '@src': './src',
+      '@tauri': './src-tauri',
+      '@base': './src/base',
+      '@comp': './src/component',
+      '@con': './src/container',
+      '@core': './src/core',
+      '@db': './src/db',
+      '@lib': './src/lib',
+      '@page': './src/page',
+      '@scss': './src/scss',
+      '@service': './src/service',
+      '@static': './src/static',
+      '@image': './src/static/image',
+      '@icon': './src/static/icon',
+      '@store': './src/store',
+      '@styled': './src/styled',
+      '@theme': './src/theme',
+      '@tool': './src/tool',
+      '@ui': './src/ui',
+    },
+  },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import '@scss/index.scss';
+import AppRoot from '@con/app-root';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = createRoot(rootEl);
   root.render(
     <StrictMode>
-      <App />
+      <AppRoot />
     </StrictMode>,
   );
 }
